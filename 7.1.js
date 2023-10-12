48a123244389b377df9ecc94f9941be329027407
Sanderkldvv <144031260+Sanderkldvv@users.noreply.github.com>
2023-10-12 10:04:19 +0200

    Add files via upload

---
+++ 7.1.js
@@ -0,0 +1,16 @@
+let budget = 100;
+
+let productprijs = 60;
+
+let saldo = document.getElementById ("Resultaat")
+
+if (budget => productprijs)
+let over = budget - productprijs
+saldo.innerHTML = "U heeft genoeg geld!" + over + "is er over van :" + budget + "euro";
+saldo.style.color = "green"
+}
+    else {
+        saldo.innerHTML= "Helaas, u heeft te weinig geld!";
+        saldo.style.color = "red";
+    }
+
